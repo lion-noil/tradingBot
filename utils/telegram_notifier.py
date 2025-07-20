49a92be6c5cc7fcbcf0e3c2e06834653f60123f3
Lion noiL <108247128+lion-noil@users.noreply.github.com>
2025-07-21 00:29:55 +0900

    add tel

---
+++ utils/telegram_notifier.py
@@ -0,0 +1,20 @@
+# utils/telegram_notifier.py
+
+import requests
+
+def send_telegram_message(bot_token: str, chat_id: str, message: str):
+    if not message:
+        print("❗ 메시지 내용이 비어 있습니다. 전송하지 않습니다.")
+        return
+
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": message,
+        "parse_mode": "HTML"  # 또는 None
+    }
+    try:
+        response = requests.post(url, data=payload)
+        response.raise_for_status()
+    except Exception as e:
+        print(f"❗ Telegram 예외 발생: {e}")
