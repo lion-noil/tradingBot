55b0a75d58d4f609213ed84b8661e679a7eeafcc
Lion noiL <108247128+lion-noil@users.noreply.github.com>
2025-12-12 02:08:48 +0900

    add mt5 test version

--- app/main_only_mt.py
+++ app/main_only_mt.py
@@ -209,4 +209,4 @@ async def startup_event():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("app.main_only_mt:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run("app.main_only_mt:app", host="127.0.0.1", port=8001, reload=False)
