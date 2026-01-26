c7af5d8a4ea08bf06a5dffae6e31db6f387cd08e
Lion noiL <108247128+lion-noil@users.noreply.github.com>
2026-01-27 02:55:07 +0900

    edit for log floating point

--- bots/trade_config.py
+++ bots/trade_config.py
@@ -333,8 +333,7 @@ def make_bybit_config(
     - 기존 TradeConfig 기본값을 그대로 사용하면서, 필요시 인자만 살짝 바꿔서 재사용.
     """
     if symbols is None:
-        # symbols = ("BTCUSDT","ETHUSDT","SOLUSDT","XRPUSDT","XAUTUSDT")
-        symbols = ("BTCUSDT",)
+        symbols = ("BTCUSDT","ETHUSDT","SOLUSDT","XRPUSDT","XAUTUSDT")
 
     if entry_percent_by_symbol is None:
         entry_percent_by_symbol = {
